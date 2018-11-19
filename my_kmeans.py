d7ab9a2705da9a638a23e383ad78fcc9ba4e5cdc
Bilge <bilgus35@gmail.com>
2018-11-19 14:01:57 +0100

    Update my_kmeans.py

--- my_kmeans.py
+++ my_kmeans.py
@@ -2,7 +2,6 @@ import numpy as np
 import scipy, scipy.spatial
 
 def my_kmeans(data, k):
-    # TODO:
     # The variable 'data' contains data points in its rows.
     # Initilize 'k' centers randomized.  Afterwards apply Floyd's algorithm
     # until convergence to get a solution of the k-means problem.  Utilize
